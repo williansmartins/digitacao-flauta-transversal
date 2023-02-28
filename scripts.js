d36a29b963e070fdd710ebd3a4066d5da83b7cbc
Willians Martins <professorwillians@gmail.com>
2023-02-28 17:26:53 -0300

    add angular

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-//alert(123);
+var app = angular.module('app', []);
+
+app.controller('Controller1', function($scope, $http){
+    $scope.digitacoes = [];
+
+    $http.get('modelo.json').then(function(response){
+        $scope.digitacoes = response.data.digitacoes;
+    });
+});
